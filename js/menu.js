46035e1b83683c940e34835ffdbc560fde6254e2
antonhann <antonha016@gmail.com>
2023-06-06 02:05:09 -0400

    added style to the buttons
    need to fix its responsiveness

--- js/menu.js
+++ js/menu.js
@@ -12,28 +12,51 @@ eatButtons.forEach(button => {
         foodContent.style.display = "block"
     })
 })
-const navbarHeight = document.querySelector("nav").offsetHeight;
+const navbarHeight = document.querySelector("nav").offsetHeight + 100;
 const noodleButton = document.querySelector("#noodle")
 noodleButton.addEventListener("click", () => {
     const noodleSection = document.querySelector("#noodle-section")
     var targetPosition = noodleSection.offsetTop - navbarHeight;
+    removeActiveButton()
+    noodleButton.classList.add("select")
     window.scrollTo({ top: targetPosition, behavior: "smooth"})
 })
 const appButton = document.querySelector("#appetizer")
 appButton.addEventListener("click", () => {
     const appSection = document.querySelector("#appetizer-section")
     var targetPosition = appSection.offsetTop - navbarHeight;
+    removeActiveButton()
+    appButton.classList.add("select")
     window.scrollTo({ top: targetPosition, behavior: "smooth"})
 })
 const riceButton = document.querySelector("#rice")
 riceButton.addEventListener("click", () => {
     const riceSection = document.querySelector("#rice-section")
     var targetPosition = riceSection.offsetTop - navbarHeight;
+    removeActiveButton()
+    riceButton.classList.add("select")
     window.scrollTo({ top: targetPosition, behavior: "smooth"})
 })
 const entreeButton = document.querySelector("#entree")
 entreeButton.addEventListener("click", () => {
     const entreeSection = document.querySelector("#entree-section")
     var targetPosition = entreeSection.offsetTop - navbarHeight;
+    removeActiveButton()
+    entreeButton.classList.add("select")
     window.scrollTo({ top: targetPosition, behavior: "smooth"})
 })
+const bevButton = document.querySelector(".bev")
+bevButton.addEventListener("click", () => {
+    const bevSection = document.querySelector("#bev-content")
+    var targetPosition = bevSection.offsetTop - navbarHeight;
+    removeActiveButton()
+    bevButton.classList.add("select")
+    window.scrollTo({ top: targetPosition, behavior: "smooth"})
+})
+
+function removeActiveButton() {
+    const buttons = document.querySelectorAll(".menu-buttons a")
+    buttons.forEach(button => {
+        button.classList.remove("select")
+    })
+}
